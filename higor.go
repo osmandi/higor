ca18f6ef414667b2587ec3d0194b730e301a7bd0
Osmandi <osmandigt@gmail.com>
2020-11-23 21:53:41 -0500

    First intent to get dataFrame, new starting :)

---
+++ higor.go
@@ -0,0 +1,104 @@
+package higor
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+	"text/tabwriter"
+)
+
+// DataFrame contain the dataFrames methods and atributes
+type DataFrame struct {
+	Columns  []string
+	Values   [][]string
+	Shape    [2]int
+	Sep      rune
+	Filename string
+}
+
+// PrintHelloHigor To get greets from higor library
+func PrintHelloHigor() string {
+	return "Hello from higor"
+}
+
+func printDataFrame(columns []string, values [][]string) {
+	const padding = 3
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, padding, ' ', tabwriter.TabIndent)
+
+	// Print Header
+	header := strings.Join(columns, "\t")
+	fmt.Fprintf(w, "%s\t%v\n", " ", header)
+
+	// Print values
+	var value string
+	for i, v := range values {
+		value = strings.Join(v[:], "\t")
+		fmt.Fprintf(w, "%d\t%v\n", i, value)
+	}
+
+	w.Flush()
+}
+
+// Head to get the first 5 rows
+func (df DataFrame) Head() {
+	printDataFrame(df.Columns, df.Values[:5])
+}
+
+// String Return string to print it
+func (df DataFrame) String() string {
+	printDataFrame(df.Columns, df.Values)
+
+	return ""
+}
+
+// NewDataFrame set default variables to read dataframe
+func NewDataFrame(filename string) *DataFrame {
+	return &DataFrame{
+		Sep:      ',',
+		Filename: filename,
+	}
+}
+
+// ReadCSV to read CSV files
+func (df *DataFrame) ReadCSV() {
+
+	// Open the iris dataset file
+	f, err := os.Open(df.Filename)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	reader := csv.NewReader(f)
+	// Set custom parameters
+	reader.Comma = df.Sep
+	var values [][]string
+
+	for {
+		var lines []string
+		// Read in a row. Check if we are at the end of the line
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+
+		for _, value := range record {
+			lines = append(lines, value)
+
+		}
+
+		values = append(values, lines)
+
+	}
+
+	// Set DataFrame parameters
+	df.Columns = values[0]
+	df.Values = values[1:]
+	df.Shape = [2]int{len(df.Columns), len(df.Values)}
+
+}
