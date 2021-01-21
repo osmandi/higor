248343e57d770d264b92720ce43ebb0016b3bba5
Osmandi <osmandigt@gmail.com>
2021-01-21 15:44:21 -0500

    Update first example

--- examples/load_data.go
+++ examples/load_data.go
@@ -7,5 +7,8 @@ import (
 func main() {
 	dfHigor := higor.NewDataFrame("examples/data/example1.csv")
 	dfHigor.ReadCSV()
+
+	// Print the first 2 rows, (you can input another rows numbers)
 	dfHigor.Head(2)
+
 }
