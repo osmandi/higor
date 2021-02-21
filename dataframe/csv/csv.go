ef7d3697bb249222aae642e5c06af68c56c097cb
Osmandi <osmandigt@gmail.com>
2021-02-21 15:01:17 -0500

    Documentation the code

--- dataframe/csv/csv.go
+++ dataframe/csv/csv.go
@@ -16,24 +16,28 @@ type CSV struct {
 
 type CSVOption func(c *CSV)
 
+// Sep CSV separator in rune type: ',', ';', '|', etc...
 func Sep(separator rune) CSVOption {
 	return func(c *CSV) {
 		c.Sep = separator
 	}
 }
 
+// Line A line
 func Line(textLine string) CSVOption {
 	return func(c *CSV) {
 		c.LineString = textLine
 	}
 }
 
+// LazyQuotes Interpreting internal quotes
 func LazyQuotes(lazy bool) CSVOption {
 	return func(c *CSV) {
 		c.LazyQuotes = lazy
 	}
 }
 
+// CSVReadHeader Read the first column has a header
 func CSVReadHeader(opts ...CSVOption) []string {
 	csvInternal := &CSV{}
 	csvInternal.Sep = ','
