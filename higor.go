3f9b54b3765c52105f88a722808d376fdbb05426
Osmandi <osmandigt@gmail.com>
2021-08-02 19:00:47 -0500

    Implement ReadCSV basic

--- higor.go
+++ higor.go
@@ -1,3 +1,38 @@
 package higor
 
+import (
+	"encoding/csv"
+	"os"
+
+	c "github.com/osmandi/higor/csv"
+	"github.com/osmandi/higor/dataframe"
+)
+
 const Version string = "v0.5.0"
+
+// ReadCSV Read a file with CSV format
+func ReadCSV(filename string, csvOptions ...c.CSVOptions) dataframe.DataFrame {
+	df := dataframe.NewDataFrame()
+
+	csvFile, err := os.Open(filename)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer csvFile.Close()
+
+	csvLines, err := csv.NewReader(csvFile).ReadAll()
+	if err != nil {
+		panic(err)
+	}
+
+	df.Columns = csvLines[0]
+	df.Shape = [2]int{len(csvLines[1:]), len(csvLines[0])}
+
+	for _, v := range csvLines[1:] {
+		df.AddLine(v)
+	}
+
+	return df
+}
--- higor_test.go
+++ higor_test.go
@@ -2,8 +2,11 @@ package higor
 
 import (
 	"os/exec"
+	"reflect"
 	"strings"
 	"testing"
+
+	"github.com/osmandi/higor/dataframe"
 )
 
 func TestVersion(t *testing.T) {
@@ -18,4 +21,28 @@ func TestVersion(t *testing.T) {
 	}
 }
 
-// TODO: higor.ReadCSV() implementation
+func TestReadCSV(t *testing.T) {
+
+	// Normal comparation
+	inputData := [][]string{[]string{"name", "age"}, []string{"pepito", "21"}, []string{"juanito", "22"}, []string{"pepita", "23"}, []string{"juanita", "24"}}
+	dfExpected := dataframe.NewDataFrame()
+	dfExpected.Columns = inputData[0]
+	dfExpected.Shape = [2]int{4, 2}
+	for _, v := range inputData[1:] {
+		dfExpected.AddLine(v)
+	}
+
+	dfResult := ReadCSV("csv_examples/simple.csv")
+
+	if !reflect.DeepEqual(dfExpected, dfResult) {
+		t.Errorf("Both DataFrame are different but equal expected.\nExpected: %+v\nResult: %+v", dfExpected, dfResult)
+	}
+
+	// TODO: Custom Sep comparation
+	// TODO: LazyQuotes comparation
+	// TODO: Custom NaN comparation
+	// TODO: Custom Datetime comparation
+	// TODO: Without columns
+	// TODO: More rows than columns
+
+}
