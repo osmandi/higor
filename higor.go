3ae2aab3e19827bc1fd8634d6ee6076c3b3936ff
osmandi <osmandigt@gmail.com>
2021-10-16 14:51:11 -0500

    Implement structure by columns

--- higor.go
+++ higor.go
@@ -1,16 +1,21 @@
 package higor
 
+import (
+	"encoding/csv"
+	"log"
+	"os"
+
+	c "github.com/osmandi/higor/csv"
+	"github.com/osmandi/higor/dataframe"
+)
+
 const Version string = "v0.5.0"
 
-/*
-
 // ReadCSV Read a file with CSV format
-func ReadCSV(filename string, csvOptions ...c.CSVOptions) dataframe.DataFrame {
+func ReadCSV(filename string, schema map[int]interface{}, csvOptions ...c.CSVOptions) dataframe.DataFrame {
 
 	csvInternal := &c.CSV{}
 	// Default values
-	csvInternal.Sep = ','
-	csvInternal.NaNLayout = ""
 
 	for _, csvOption := range csvOptions {
 		csvOption(csvInternal)
@@ -26,31 +31,12 @@ func ReadCSV(filename string, csvOptions ...c.CSVOptions) dataframe.DataFrame {
 
 	records := csv.NewReader(csvFile)
 
-	// Set options
-	records.Comma = csvInternal.Sep
-
-	csvLines := [][]string{}
-
-	for {
-		line, err := records.Read()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			if *&err.(*csv.ParseError).Err == csv.ErrFieldCount {
-				// More rows than columns. Return only validated rows
-				line = line[:len(csvLines[0])]
-			} else {
-				panic(err)
-			}
-		}
-
-		csvLines = append(csvLines, line)
-
+	csvLines, err := records.ReadAll()
+	if err != nil {
+		log.Fatal(err)
 	}
-	df := dataframe.NewDataFrame(csvLines, csvInternal.NaNLayout)
+
+	df := dataframe.NewDataFrame(csvLines[1:], csvLines[0], schema, "")
 
 	return df
 }
-
-*/
